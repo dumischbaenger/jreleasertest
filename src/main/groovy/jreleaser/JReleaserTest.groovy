4e55afd29dccfb4a005b4a553b1c4967e188a558
Bernhard Donaubauer <bdonauba@gmail.com>
2023-02-07 20:38:05 +0100

    initial commit

---
+++ src/main/groovy/jreleaser/JReleaserTest.groovy
@@ -0,0 +1,27 @@
+package jreleaser
+
+import groovy.util.logging.Log4j2
+
+/**
+ * JReleaserTest
+ * 
+ * @author bdonauba
+ *
+ */
+@Log4j2
+class JReleaserTest {
+
+
+  /**
+   * Hauptprogramm
+   * 
+   * @param args
+   */
+  static main(args) {
+    log.info "program started"
+
+    println "hello jreleaser"
+ 
+    log.info "program stopped"
+  }
+}
